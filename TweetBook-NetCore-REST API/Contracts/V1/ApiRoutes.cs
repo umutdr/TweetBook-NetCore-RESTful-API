01394a93283733021187a86f8473e6edcb6a1f2f
Umut <umutdr98@gmail.com>
2020-01-04 13:55:12 +0300

    Video 9 Done

--- TweetBook-NetCore-REST API/Contracts/V1/ApiRoutes.cs
+++ TweetBook-NetCore-REST API/Contracts/V1/ApiRoutes.cs
@@ -18,7 +18,7 @@ namespace TweetBook_NetCore_REST_API.Contracts.V1
             public const string Get = Base + "/posts/{postId}";
             public const string Update = Base + "/posts/{postId}";
             public const string Delete = Base + "/posts/{postId}";
-            public const string Create = Base + "/create";
+            public const string Create = Base + "/posts";
         }
     }
 }
--- TweetBook-NetCore-REST API/Installers/MVCInstaller.cs
+++ TweetBook-NetCore-REST API/Installers/MVCInstaller.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using Swashbuckle.AspNetCore.Swagger;
+using TweetBook_NetCore_REST_API.Options;
 
 namespace TweetBook_NetCore_REST_API.Installers
 {
@@ -13,12 +17,54 @@ namespace TweetBook_NetCore_REST_API.Installers
     {
         public void InstallServices(IServiceCollection services, IConfiguration configuration)
         {
+            var jwtSettings = new JwtSettings();
+            configuration.Bind(key: nameof(jwtSettings), instance: jwtSettings);
+            services.AddSingleton(jwtSettings);
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtSettings.Secret)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    RequireExpirationTime = false,
+                    ValidateLifetime = true
+
+                };
+            });
 
             services.AddSwaggerGen(x =>
             {
                 x.SwaggerDoc("v1", new Info { Title = "TweetBook", Version = "v1" });
+
+                var security = new Dictionary<string, IEnumerable<string>>
+                {
+                    {"Bearer",new string[0]}
+                };
+
+                x.AddSecurityDefinition("Bearer", new ApiKeyScheme
+                {
+                    Description = "Authorization",
+                    In = "header",
+                    Type = "apiKey"
+                });
+
+                x.AddSecurityRequirement(security);
+
             });
+
+           
         }
     }
 }
